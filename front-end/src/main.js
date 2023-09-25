f7e75eaa65826de4e8ed3cda9ebf4d85e4219558
Timon Schill <timonschill@googlemail.com>
2023-09-25 15:19:40 -0400

    Add temp data to products page

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,22 +1,29 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import * as VueRouter from 'vue-router'
-import ShoppingCartPage from './pages/ShoppingCartPage.vue';
-import ProductsPage from './pages/ProductsPage.vue';
-import ProductDetailPage from './pages/ProductDetailPage.vue';
+import { createApp } from "vue";
+import App from "./App.vue";
+import * as VueRouter from "vue-router";
+import ShoppingCartPage from "./pages/ShoppingCartPage.vue";
+import ProductsPage from "./pages/ProductsPage.vue";
+import ProductDetailPage from "./pages/ProductDetailPage.vue";
+import "./main.css";
 
 createApp(App)
-.use(VueRouter.createRouter({
-  history: VueRouter.createWebHistory(process.env.BASE_URL),
-  routes: [{
-    path: '/cart',
-    component: ShoppingCartPage,
-  }, {
-    path: '/products',
-    component: ProductsPage,
-  }, {
-    path: '/products/:productId',
-    component: ProductDetailPage,
-  }]
-}))
-.mount('#app')
+  .use(
+    VueRouter.createRouter({
+      history: VueRouter.createWebHistory(process.env.BASE_URL),
+      routes: [
+        {
+          path: "/cart",
+          component: ShoppingCartPage,
+        },
+        {
+          path: "/products",
+          component: ProductsPage,
+        },
+        {
+          path: "/products/:productId",
+          component: ProductDetailPage,
+        },
+      ],
+    })
+  )
+  .mount("#app");
