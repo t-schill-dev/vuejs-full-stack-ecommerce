7b9d543c064abffcdf434cd08dca2a73e28dcc19
Timon Schill <timonschill@googlemail.com>
2023-09-27 15:35:48 -0400

    Creating endpoints

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,10 +1,41 @@
 //Entry point backend
-import express from "express";
+import express, { json } from "express";
+import { cartItems as cartItemRaw, products as productsRaw } from "./temp-data";
+
+let cartItems = cartItemRaw;
+let products = productsRaw;
 
 const app = express();
+app.use(express.json()); //parse request body when body undefined
 
 app.get("/hello", (req, res) => {
   res.send("Hello");
+});
+
+app.get("/products", (req, res) => {
+  res.json(products);
+});
+
+app.get("/cart", (req, res) => {
+  res.json(cartItems);
+});
+
+app.get("/products/:productId", (req, res) => {
+  const productId = req.params.productId;
+  const product = products.find((product) => product.id === productId);
+  res.json(product);
+});
+
+app.post("/cart", (req, res) => {
+  const productId = req.body.id;
+  const product = products.find((product) => product.id === productId);
+  res.json(cartItems);
+});
+
+app.delete("/cart/:productId", (req, res) => {
+  const productId = req.params.productId;
+  cartItems = cartItems.filter((product) => product.id !== productId);
+  res.json(cartItems);
 });
 
 app.listen(8000, () => {
